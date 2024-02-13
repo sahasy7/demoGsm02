04ca1c434aa7c25f7b6eb954e11d94cc6e82e9e6
sahasy7 <74914769+sahasy7@users.noreply.github.com>
2024-02-13 11:00:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llama_index.core import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader
 
 st.set_page_config(page_title="Chat with the GSM mall", page_icon="🏬", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
